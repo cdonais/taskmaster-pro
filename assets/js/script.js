9567c5fa6f39f0c95fd9d71de01278350c0ac5ea
Carly Donais <carly.donais@gmail.com>
2022-05-15 13:15:16 -0400

    drag and drop featur

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,7 +165,20 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
     $(this).replaceWith(taskSpan);
 });
-
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
+});
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -173,6 +186,53 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate",this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over",event.target);
+  },
+  out: function(event){
+    console.log("out", event.target);
+  },
+  update: function(event) {
+  var tempArr=[];
+
+  //loop over current set of children in sortable list
+  $(this).children().each(function(){
+    var text=$(this)
+      .find("p")
+      .text()
+      .trim();
+    
+    var date=$(this)
+      .find("span")
+      .text()
+      .trim();
+    
+    var arrName= $(this)
+      .attr("id")
+      .replace("list-", "");
+      
+    tasks[arrName]=tempArr;
+    saveTasks();  
+//add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });  
+});  
+console.log(tempArr);
+  }
 });
 
 // load tasks for the first time
